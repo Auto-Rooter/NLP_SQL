760f9d805975a27d4b9791f8c7e435eb310dc475
Hadi Assalem <hadi.assalem@gmail.com>
2025-03-23 00:07:05 +0100

    Feat(BE/DB): Create/Run ChartInfo Entity migration

---
+++ src/entities/chartInfo.entity.ts
@@ -0,0 +1,50 @@
+import { Column, CreateDateColumn, Entity, Index, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Datasource } from './datasource.entity';
+
+@Entity()
+@Index(['datasourceId'])
+@Index(['userId'])
+export class ChartInfo {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  chartName: string;
+
+  @ManyToOne(() => Datasource)
+  @JoinColumn({ name: 'datasourceId' })
+  datasource: Datasource
+
+  @Column()
+  datasourceId: string;
+
+  @Column({ nullable: true })
+  databaseUrl: string;
+
+  @Column()
+  chartType: string;
+
+  @Column()
+  xAxis: string;
+
+  @Column()
+  yAxis: string;
+
+  @Column()
+  prompt: string;
+
+  @Column('text')
+  queryData: string;
+
+  @Column('text')
+  chartData: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date
+}
